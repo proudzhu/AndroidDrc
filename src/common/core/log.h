891ade59612d42724a0e1297e6b9276c307f1771
proudzhu <proudzhu.fdu@gmail.com>
2016-02-29 16:42:17 +0800

    now build with vs2010

---
+++ src/common/core/log.h
@@ -0,0 +1,26 @@
+#ifndef LE_FX_ENGINE_DSP_CORE_LOG_H_
+#define LE_FX_ENGINE_DSP_CORE_LOG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LOGV ALOGV
+#define LOGD ALOGD
+#define LOGE ALOGE
+#define LOGW ALOGW
+#define LOGI ALOGI
+
+#define ALOGV printf
+#define ALOGD printf
+#define ALOGE printf
+#define ALOGW printf
+#define ALOGI printf
+
+#define LoggerError printf
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
